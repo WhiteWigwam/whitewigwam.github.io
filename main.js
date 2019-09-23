1bcf8d98e1fce65d140af86e0c3de867dd9c1117
whitewigwam <loskSergg@gmail.com>
2019-09-23 09:23:58 +0300

    pure 0.1

---
+++ main.js
@@ -0,0 +1,16 @@
+
+   function change()
+{
+    
+    var h = window.innerHeight;
+	var w = window.innerWidth;
+
+	document.getElementById("row1d2").innerHTML=w; 
+	document.getElementById("row2d2").innerHTML=h; 
+
+
+
+
+
+
+}
